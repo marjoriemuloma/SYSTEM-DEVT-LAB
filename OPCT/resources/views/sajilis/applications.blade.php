66cfc179dab50d4bd78948d06cce37545f7469fb
marjoriemuloma <marjoriemuloma1@gmail.com>
2022-10-12 09:34:20 +0300

    Deleted

--- OPCT/resources/views/sajilis/applications.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- View stored in resources/views/greeting.blade.php -->
- 
-<html>
-    <body>
-        <h1>Hello, {{ $name }}</h1>
-        <h1>You are an {{ $occupation }}</h1>
-    </body>
-</html>
