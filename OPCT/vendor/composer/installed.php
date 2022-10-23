76df22e37302135f859b07f495a2154db1cda456
marjoriemuloma <marjoriemuloma1@gmail.com>
2022-10-23 11:30:36 +0300

    reinstalled the vite dependancy

--- OPCT/vendor/composer/installed.php
+++ OPCT/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'eb5029a3eb4f08bb24694e578737c74a98d8ed75',
+        'reference' => '73a3898f477d46f45ebc2e01c3adf4b27c82dc2e',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -484,7 +484,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'eb5029a3eb4f08bb24694e578737c74a98d8ed75',
+            'reference' => '73a3898f477d46f45ebc2e01c3adf4b27c82dc2e',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
